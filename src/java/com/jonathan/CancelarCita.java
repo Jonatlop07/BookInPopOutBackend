5bb38b421c9354d1fa6edbf43e0e7f29d1ea78ad
Jonathan Lopez <jonatlop@gmail.com>
2020-07-06 11:43:39 -0500

    Terminados los servlets

--- src/java/com/jonathan/CancelarCita.java
+++ src/java/com/jonathan/CancelarCita.java
@@ -13,11 +13,10 @@ public class CancelarCita extends HttpServlet {
     public CancelarCita() {
 	super();
     }
-
+    
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-        
-	String id_usuario = request.getParameter("id");
+        String id_usuario = request.getParameter("id");
         int hora = Integer.parseInt(request.getParameter("hora"));
         int minuto = Integer.parseInt(request.getParameter("minuto"));
 
@@ -29,7 +28,7 @@ public class CancelarCita extends HttpServlet {
             e.printStackTrace();
 	}
     }
-
+    
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
         doGet(request, response);
